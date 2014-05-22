3f6d24ccf0308b97ab047874ef13563d06d748b0
Cagatay Demiralp <cagatay@cs.stanford.edu>
2014-05-21 18:46:23 -0700

    Update runProduction.sh

--- exp/shape/l5/runProduction.sh
+++ exp/shape/l5/runProduction.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 #
 #
-# Loads the HIT $STUDY_HOME/exp/shape/l5/shape-l5 to run in the Amazon Mechanical Turk site
-# (sandbox or production, depending on the setting of service_url in $MTURKCLT_HOME/bin/mturk.properties)
+# Loads the HIT $STUDY_HOME/exp/shape/l5/shape-l5 to run in the Amazon Mechanical Turk production site
+# (*** assumes service_url in $MTURKCLT_HOME/bin/mturk.properties is already set to the production site ***).
 #
 #
 $MTURKCLT_HOME/bin/loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 \
